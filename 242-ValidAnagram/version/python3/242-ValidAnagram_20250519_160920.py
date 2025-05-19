9a27872a4d3ceddbb877a13b09f0be46c949dfae
Bolin Lin <linbolin1230@gmail.com>
2025-05-19 16:09:20 -0400

    Create file

---
+++ 242-ValidAnagram/version/python3/242-ValidAnagram_20250519_160920.py
@@ -0,0 +1,22 @@
+# Last updated: 5/19/2025, 4:09:20 PM
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+
+        map_char = {}
+        for char in s:
+            if char in map_char:
+                map_char[char] += 1
+            else:
+                map_char[char] = 1
+        
+        for char in t:
+            if char in map_char:
+                map_char[char] -= 1
+                if map_char[char] == 0:
+                    del map_char[char]
+            else:
+                return False
+
+        return True
