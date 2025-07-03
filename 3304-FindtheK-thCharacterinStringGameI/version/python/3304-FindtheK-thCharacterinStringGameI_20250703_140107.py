3f6468e7d9c827aaca563a5c5abee0106043cf1c
Bolin Lin <linbolin1230@gmail.com>
2025-07-04 02:01:08 +0800

    Create 3304-FindtheK-thCharacterinStringGameI

---
+++ 3304-FindtheK-thCharacterinStringGameI/version/python/3304-FindtheK-thCharacterinStringGameI_20250703_140107.py
@@ -0,0 +1,18 @@
+# Last updated: 7/3/2025, 2:01:07 PM
+class Solution:
+    def kthCharacter(self, k: int) -> str:
+        # string=["a"]
+        # while len(string)<=k:
+        #     next_string=[chr(((ord(c)-ord('a')+1)%26)+ord('a')) for c in string]
+        #     string+=next_string
+        # return string[k-1]
+
+        ans = 0
+        while k != 1:
+            t = k.bit_length() - 1
+            if (1 << t) == k:
+                t -= 1
+            k -= 1 << t
+            ans += 1
+        return chr(ord("a") + ans)
+        
