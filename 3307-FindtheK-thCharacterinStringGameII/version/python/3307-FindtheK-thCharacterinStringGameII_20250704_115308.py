e49f6e6937a9b1bc2d928c90dbbbc720823d8782
Bolin Lin <linbolin1230@gmail.com>
2025-07-04 23:53:08 +0800

    Create 3307-FindtheK-thCharacterinStringGameII

---
+++ 3307-FindtheK-thCharacterinStringGameII/version/python/3307-FindtheK-thCharacterinStringGameII_20250704_115308.py
@@ -0,0 +1,12 @@
+# Last updated: 7/4/2025, 11:53:08 AM
+class Solution:
+    def kthCharacter(self, k: int, operations: List[int]) -> str:
+        ans = 0
+        while k != 1:
+            t = k.bit_length() - 1
+            if (1 << t) == k:
+                t -= 1
+            k -= 1 << t
+            if operations[t]:
+                ans += 1
+        return chr(ord('a') + (ans % 26))
