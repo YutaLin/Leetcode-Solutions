f760c412a8251fef4ef3af7d4143e2944b55b522
Bolin Lin <linbolin1230@gmail.com>
2025-07-16 22:40:02 -0400

    Create 69-Sqrt(x)

---
+++ 69-Sqrt(x)/version/python/69-Sqrt(x)_20250716_224002.py
@@ -0,0 +1,19 @@
+# Last updated: 7/16/2025, 10:40:02 PM
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        if x < 2:
+            return x
+
+        left, right = 2, x // 2
+
+        while left <= right:
+            pivot = left + (right - left) // 2
+            num = pivot * pivot
+            if num > x:
+                right = pivot - 1
+            elif num < x:
+                left = pivot + 1
+            else:
+                return pivot
+
+        return right
