7838313a4ab77f7546e406d27ad8a9a98fe0b3f2
Bolin Lin <linbolin1230@gmail.com>
2026-01-27 18:17:27 -0500

    Add 0198-house-robber

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,12 +1,14 @@
-class Solution:
-    def rob(self, nums: List[int]) -> int:
-        n = len(nums)
-        dp = [0] * (n + 2)
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        n = len(nums)
+        # dp = [0] * (n + 2)
+        newF = 0
+        for i, x in enumerate(nums):
+            # dp[i+2] = max(dp[i+1], dp[i] + nums[i])
 
-        for i, x in enumerate(nums):
-            dp[i+2] = max(dp[i+1], dp[i] + nums[i])
+# f(i) = max(f(i-1), f(i-2) + nums[i])
 
-        return dp[n+1]
-
-# f(i) = max(f(i-1), f(i-2) + nums[i])
-# 
+        return dp[n+1]
+        f0 = 0
+        f
+# 
