2cf09ee9b3fa914e2e2e8d4c96dce005f69e439c
Bolin Lin <linbolin1230@gmail.com>
2026-01-30 18:09:20 -0500

    Time: 17 ms (35.85%), Space: 24.2 MB (6.73%)

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,14 +1,12 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        
         dic = defaultdict(list)
-        for string in strs:
+        for s in strs:
             char_list = [0] * 26
-            
-            for char in string:
-                char_list[ord(char) - ord('a')] += 1
-             
-            dic[tuple(char_list)].append(string)
+            for c in s:
+                char_list[ord(c) - ord('a')] += 1
 
+            dic[tuple(char_list)].append(s)
+        
         return list(dic.values())
-
-            
