943649c82fcb9265de656a4ca6f7d36f58c6d8f5
Bolin Lin <linbolin1230@gmail.com>
2026-06-30 20:31:20 -0400

    Time: 0 ms (100%), Space: 19.3 MB (39.91%)

--- 1929-concatenation-of-array/1929-concatenation-of-array.py
+++ 1929-concatenation-of-array/1929-concatenation-of-array.py
@@ -1,7 +1,3 @@
 class Solution:
     def getConcatenation(self, nums: List[int]) -> List[int]:
-        ans = nums.copy()
-        for num in nums:
-            ans.append(num)
-
-        return ans
+        return nums * 2
