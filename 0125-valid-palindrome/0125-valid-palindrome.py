493b07c37688914546767f1a87bda6b2daadbbaf
Bolin Lin <linbolin1230@gmail.com>
2026-06-30 20:38:50 -0400

    Time: 7 ms (81.11%), Space: 19.6 MB (77.06%)

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,33 +1,18 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        if not s:
-            return True
-        l = 0
-        r = len(s) - 1
-
-        while l < r:
-            while l < r and not s[l].isalnum():
-                l += 1
-            while l < r and not s[r].isalnum():
-                r -= 1
-
-            if s[l].lower() != s[r].lower():
+        n = len(s)
+        left = 0
+        right = n - 1
+        
+        while left < right:
+            while left < right and not s[left].isalnum():
+                left += 1
+            while left < right and not s[right].isalnum():
+                right -= 1
+            if s[left].lower() != s[right].lower():
                 return False
-            l += 1
-            r -= 1
+
+            left += 1
+            right -= 1
 
         return True
-
-    
-
-
-
-
-
-### "abcba" 
-### "a,b,c,ba"
-### "abcd dcba"
-### " abc   ,  ba "
-## " "
-
-# empty String
